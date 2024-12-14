9eec56bb86d9b52bba2789b803a28fc98324e39a
SOUMEN MONDAL <151416831+Soumen-JavaDev@users.noreply.github.com>
2024-12-15 00:28:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,22 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+	arr.sort((a,b)=>a-b)
+
+	let total=0;
+	while(arr.length>1){
+		let first=arr.shift() ;
+		let second=arr.shift()
+
+		let cost=first+second;
+
+		total +=cost;
+		arr.push(cost);
+
+		arr.sort((a,b)=>a-b)
+	}
+
+	return total;
   
 }
 
